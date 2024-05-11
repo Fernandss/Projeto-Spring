0cb65b8a0eb3feef138efcc790cf20cda4e61de2
Arthur Vinicius da Costa Fernandes <arthur.fernandes@totvs.com.br>
2024-05-11 20:56:29 -0300

    envio dos arquivos

---
+++ Downloads/springboot/springboot/src/main/java/com/projeto/integrador/springboot/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.projeto.integrador.springboot.controller;
+
+import com.projeto.integrador.springboot.entity.User;
+import com.projeto.integrador.springboot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userService.save(user);
+        return ResponseEntity.ok(savedUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+}
---
+++ Downloads/springboot/springboot/src/main/java/com/projeto/integrador/springboot/service/UserService.java
@@ -0,0 +1,20 @@
+package com.projeto.integrador.springboot.service;
+
+import com.projeto.integrador.springboot.entity.User;
+import com.projeto.integrador.springboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+}
